277b2f87df19700fc47ea379218def468e1ac757
Magicoo51889 <jackot11f@gmail.com>
2021-09-23 22:24:56 +0100

    Updated to current code

---
+++ script.js
@@ -0,0 +1,14 @@
+const outputHeading = document.getElementById('result');
+const submitButton = document.getElementById('cmdMultiply');
+
+function multiply(e) {
+	e.preventDefault();
+
+	const number1 = document.forms['maths']['number1'].value
+	const number2 = document.forms['maths']['number2'].value
+	const multiplied = number1 * number2
+
+	outputHeading.innerHTML = number1 + " x " + number2 + " = " + multiplied;
+}
+
+submitButton.addEventListener('click', multiply);
