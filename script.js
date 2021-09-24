19e32dabc8e954f73cc62dbcf632ed8ef92f0a29
Magicoo51889 <jackot11f@gmail.com>
2021-09-24 10:46:14 +0100

    Update js, css, html nav bar

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const submitButton = document.getElementById('cmdMultiply');
 function multiply(e) {
 	e.preventDefault();
 
-	const number1 = document.forms['maths']['number1'].value
-	const number2 = document.forms['maths']['number2'].value
+	const number1 = parseInt(document.forms['maths']['number1']).value
+	const number2 = parseInt(document.forms['maths']['number2']).value
 	const multiplied = number1 * number2
 
 	outputHeading.innerHTML = number1 + " x " + number2 + " = " + multiplied;
