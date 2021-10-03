d0d26bebe358dc23497c053a87f34c000f30dc2c
Magicoo51889 <jackot11f@gmail.com>
2021-10-03 13:32:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const submitButton = document.getElementById('cmdMultiply');
 function multiply(e) {
 	e.preventDefault();
 
-	const number1 = parseInt(document.forms['maths']['number1']).value
-	const number2 = parseInt(document.forms['maths']['number2']).value
+	const number1 = parseInt(document.forms['maths']['number1'].value)
+	const number2 = parseInt(document.forms['maths']['number2'].value)
 	const multiplied = number1 * number2
 
 	outputParagraph.innerHTML = number1 + " x " + number2 + " = " + multiplied;
